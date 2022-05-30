96848b17e3d5bf847c94e2f63a66c6321e639948
powerfulman86 <dba.ahmedmaher@gmail.com>
2022-05-30 16:03:49 +0200

    UPD

--- models/stock_inventory.py
+++ models/stock_inventory.py
@@ -5,17 +5,14 @@ from odoo import api, fields, models, _
 class StockInventory(models.Model):
     _inherit = 'stock.inventory'
 
-    branch_id = fields.Many2one(
-        'res.branch',
-        string='Branch'
-    )
+    branch_id = fields.Many2one('res.branch', string='Branch')
 
     @api.model
     def create(self, vals):
         if vals.get('location_id', None):
             location = self.env['stock.location'].browse(vals.get('location_id'))
-            if location and location.pos_branch_id:
-                vals.update({'branch_id': location.pos_branch_id.id})
+            if location and location.branch_id:
+                vals.update({'branch_id': location.branch_id.id})
         if not vals.get('branch_id'):
             vals.update({'branch_id': self.env['res.branch'].sudo().get_default_branch()})
         inventory = super(StockInventory, self).create(vals)
